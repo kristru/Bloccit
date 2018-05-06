fb73fa6787cb32f6c0785b28ac2f1468bfb6afdf
kristru <krissafuentes@gmail.com>
2018-05-06 16:20:07 -0600

    add SPAM logic to Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,12 @@ class PostsController < ApplicationController
 #here we're declaring an instance variable @posts
 #and assign it a collection of Post objects using the all method provided by ActiveRecord.
     @posts = Post.all
+
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
