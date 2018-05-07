5b5372e81b84dd78359ebd690bd1321feb0a1010
kristru <krissafuentes@gmail.com>
2018-05-06 18:42:07 -0600

    create form and show post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,25 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
+  def new
+    @post = Post.new
   end
 
-  def new
+  def create
+    @post = Post.new
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body]
+    if @post.save
+      flash[:notice] = "Post was saved!"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
