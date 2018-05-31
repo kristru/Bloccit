d30b65a33a53f1f9d154decac81fa4bdf5c631fa
kristru <krissafuentes@gmail.com>
2018-05-30 20:04:09 -0600

    update the spec files for the advertisements controller

---
+++ spec/controllers/advertisements_controller_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe AdvertisementsController, type: :controller do
+
+  let(:my_advertisement) { Advertisement.create!(title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: RandomData.random_price) }
+
+
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "assigns [my_advertisement] to @advertisement" do
+      get :index
+  #because we created one posts (my_advertisement), we expect index to return an array of one item
+      expect(assigns(:advertisements)).to eq([my_advertisement])
+    end
+  end
+
+
+  describe "GET new" do
+  it "returns http success" do
+    get :new
+    expect(response).to have_http_status(:success)
+  end
+
+  # #2
+  it "renders the #new view" do
+    get :new
+    expect(response).to render_template :new
+  end
+
+  # #3
+  it "instantiates @advertisement" do
+    get :new
+    expect(assigns(:advertisement)).not_to be_nil
+  end
+  end
+
+  describe "POST create" do
+  # #4
+  it "increases the number of Advertisement by 1" do
+    expect{ post :create, params: { advertisement: { title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price } } }.to change(Advertisement,:count).by(1)
+  end
+
+  # #5
+  it "assigns the new post to @advertisement" do
+    post :create, params: { advertisement: { title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price } }
+    expect(assigns(:advertisement)).to eq Advertisement.last
+  end
+
+  # #6
+  it "redirects to the new post" do
+    post :create, params: { advertisement: { title: RandomData.random_sentence, body: RandomData.random_paragraph, price: RandomData.random_price } }
+    expect(response).to redirect_to Advertisement.last
+  end
+  end
+
+  describe "GET show" do
+   it "returns http success" do
+     get :show, params: { id: my_advertisement.id }
+     expect(response).to have_http_status(:success)
+   end
+
+   it "renders the #show view" do
+     get :show, params: { id: my_advertisement.id }
+     expect(response).to render_template :show
+   end
+
+   it "assigns my_advertisement to @advertisement" do
+     get :show, params: { id: my_advertisement.id }
+     expect(assigns(:advertisement)).to eq(my_advertisement)
+   end
+  end
+
+end
