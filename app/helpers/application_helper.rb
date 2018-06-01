04c2b3a25f25e2ffd3d55ee2dd8e013572efdc30
kristru <krissafuentes@gmail.com>
2018-05-31 19:52:13 -0600

    create form partial and update new and edit view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+#first argument is an array of errors, the second is a block
+   def form_group_tag(errors, &block)
+     css_class = 'form-group'
+     css_class << ' has-error' if errors.any?
+#calls the content_tag helper method is called. It builds the HTML and CSS
+     content_tag :div, capture(&block), class: css_class
+   end
 end
