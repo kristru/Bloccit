244761494012bd2033952f7618c65410e768a8ff
kristru <krissafuentes@gmail.com>
2018-06-01 12:44:44 -0600

    Add Spec tests and partial for Topic form

--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe AdvertisementsControllerController, type: :controller do
+RSpec.describe AdvertisementsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
