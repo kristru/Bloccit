56021df98b0967c5478de225dc884d7f0717fe2e
kristru <krissafuentes@gmail.com>
2018-06-07 12:56:59 -0600

    add user gravatar and style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
 #calls the content_tag helper method is called. It builds the HTML and CSS
      content_tag :div, capture(&block), class: css_class
    end
+
+   def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
 end
